a5edb375dcf1cfaafb619d3d4899af3029bbc38b
Benjamin Mao <benjaminmao@BenjamindeMacBook-Pro.local>
2024-05-14 23:33:41 +0800

    layout

--- web/src/App.js
+++ web/src/App.js
@@ -4,7 +4,23 @@ import { Header, Footer, Content } from 'antd/es/layout/layout';
 
 function App(){
     return(
-        <h1> test </h1>
+        <Layout>
+            <Header style={{
+                textAlign: 'center', color: '#fff', height: 64,
+                paddingInline: 50, lineHeight: '64px', backgroundColor: '#7dbcea'
+            }}> Header123123 </Header>
+
+            <Content style={{
+                textAlign: 'center', minHeight: 120, lineHeight: '120px',
+                color: '#fff', backgroundColor: '#108ee9'
+            }}> Header123123 </Content>
+
+            <Footer style={{
+                textAlign: 'center',
+                color: '#fff',
+                backgroundColor: '#7dbcea'
+            }}> Header123123 </Footer>
+        </Layout>
     );
 }
 export default App
