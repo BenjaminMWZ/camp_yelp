0471a6092db97bbc335a6ccf1c8ca6140153c756
Benjamin Mao <benjaminmao@BenjamindeMacBook-Pro.local>
2024-05-14 23:25:58 +0800

    test

---
+++ web/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout } from 'antd';
+import { Header, Footer, Content } from 'antd/es/layout/layout';
+
+function App(){
+    return(
+        <h1> test </h1>
+    );
+}
+export default App
